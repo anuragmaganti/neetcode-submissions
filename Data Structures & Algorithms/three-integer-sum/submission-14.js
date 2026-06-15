04e25bb79033b1113db86853a18a578089ca9e9b
Anurag Maganti <amaganti.dev@gmail.com>
2026-06-15 19:19:19 -0400

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let results = [];
+
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 2; i++) {
+            let j = i + 1;
+            let k = nums.length - 1;
+
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                continue;
+            }
+
+            while (j < k) {
+                let sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    results.push([nums[i], nums[j], nums[k]]);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] === nums[j - 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] === nums[k + 1]) {
+                        k--;
+                    }
+             
+                }
+            }
+        }
+        console.log(results);
+        return results;
+    }
+}
