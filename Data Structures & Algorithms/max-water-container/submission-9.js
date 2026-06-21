28e3de3e5ecefa4e5d485ed90d8f3e84569adbbd
Anurag Maganti <amaganti.dev@gmail.com>
2026-06-21 10:00:46 -0400

    Add: max-water-container - submission-9

---
+++ Data Structures & Algorithms/max-water-container/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+
+        let i = 0;
+        let j = heights.length-1;
+        let max = 0;
+
+
+        while (i<j) {
+
+            let area = Math.min(heights[i],heights[j])*(j-i);
+            max = Math.max(area,max);
+
+            if (heights[i]<heights[j]) {
+                i++;
+            } else {
+                j--;
+            }
+
+        }
+
+        return max;
+
+    }
+}
