91b9068a606ceb7da071afb22c8557c8b02548fe
Anurag Maganti <amaganti.dev@gmail.com>
2026-06-22 11:31:12 -0400

    Add: validate-parentheses - submission-22

---
+++ Data Structures & Algorithms/validate-parentheses/submission-22.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+
+        if (s.length % 2 !== 0) {
+            return false;
+        }
+
+        for (let char of s) {
+            if (char === "(" || char === "{" || char === "[") {
+                stack.push(char);
+            }
+
+            if (stack.length === 0 && (char === ")" || char === "}" || char === "]")) {
+                return false;
+            }
+
+            if (char === ")") {
+                if (stack[stack.length - 1] === "(") {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (char === "}") {
+                if (stack[stack.length - 1] === "{") {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (char === "]") {
+                if (stack[stack.length - 1] === "[") {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
